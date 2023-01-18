4a9de6cc5159e85459539d0c40980362bc77cfb2
Tyson Louth <tysonlouth@gmail.com>
2023-01-18 09:35:50 -0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const experiences = [
+    {
+      title: "Zebel",
+      date: "August 2022 to Present",
+      description: "Senior Front End Developer",
+      location: "San Francisco, CA (Remote)",
+      points: ["React", "Material UI"],
+    },
+    {
+      title: "Commerce7",
+      date: "June 2020 to August 2022",
+      description: "Senior Front End Developer",
+      location: "Vancouver, BC (Remote)",
+      points: ["React", "Redux", "Node.js", "Gatsby", "Next.js", "Styled Components"],
+    },
+    {
+      title: "Caredove",
+      date: "October 2019 to June 2020",
+      description: "Senior Front End Developer",
+      location: "Orillia, ON (Remote)",
+      points: ["React", "Redux", "Node.js", "Next.js", "Tailwind", "Styled Components"],
+    },
+    {
+      title: "Teaching.com",
+      date: "May 2019 to October 2019",
+      description: "Product Developer",
+      location: "San Juan, Puerto Rico (Remote)",
+      points: ["React", "Redux", "SASS", "Zend"],
+    },
+    {
+      title: "BitCine (CineSend)",
+      date: "June 2015 to May 2019",
+      description: "Lead Front End Developer",
+      location: "Toronto, ON",
+      points: ["React", "Redux", "Vue", "SASS", "Laravel"],
+    },
+    {
+      title: "Freelance",
+      date: "January 2013 to June 2015",
+      description: "Web Developer",
+      location: "Orillia, ON",
+      points: ["Wordpress", "Drupal", "jQuery"],
+    },
+  ];
+  
+  export const educations = [
+    {
+      title: "Pfeiffer University",
+      date: "Aug 2009 to May 2013",
+      description: "Bachelor's Degree, Computer Information Systems",
+      location: "Misenheimer, NC",
+    },
+  ];
+  
+  export const links = [
+    {
+      href: "https://github.com/tlouth19",
+      label: "GitHub",
+    },
+    {
+      href: "https://dev.to/tlouth19",
+      label: "Dev.to",
+    },
+    {
+      href: "https://ca.linkedin.com/in/tyson-louth-816b6aa9",
+      label: "LinkedIn",
+    },
+    {
+      href: "mailto:tysonlouth@gmail.com",
+      label: "Email",
+    },
+  ];
