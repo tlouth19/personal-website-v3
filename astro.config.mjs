9f033af43251258e84cab541163e3238f0fcc3f4
Tyson Louth <tysonlouth@gmail.com>
2023-01-18 09:42:45 -0500

    Added astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-    config: { applyBaseStyles: false }
-  })]
+    config: {
+      applyBaseStyles: false
+    }
+  }), image()]
 });
