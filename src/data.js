b4eb9d98b95ca191fcf92fcd6b09e32ba05f6b76
Tyson Louth <tysonlouth@gmail.com>
2023-06-07 13:29:58 -0400

    Update personal projects

--- src/data.js
+++ src/data.js
@@ -1,60 +1,81 @@
-export const experiences = [
-    {
-      title: "Zebel",
-      date: "August 2022 to Present",
-      description: "Senior Front End Developer",
-      location: "San Francisco, CA (Remote)",
-      points: ["React", "Material UI"],
-    },
-    {
-      title: "Commerce7",
-      date: "June 2020 to August 2022",
-      description: "Senior Front End Developer",
-      location: "Vancouver, BC (Remote)",
-      points: ["React", "Redux", "Node.js", "Gatsby", "Next.js", "Styled Components"],
-    },
-    {
-      title: "Caredove",
-      date: "October 2019 to June 2020",
-      description: "Senior Front End Developer",
-      location: "Orillia, ON (Remote)",
-      points: ["React", "Redux", "Node.js", "Next.js", "Tailwind", "Styled Components"],
-    },
-    {
-      title: "Teaching.com",
-      date: "May 2019 to October 2019",
-      description: "Product Developer",
-      location: "San Juan, Puerto Rico (Remote)",
-      points: ["React", "Redux", "SASS", "Zend"],
-    },
-    {
-      title: "BitCine (CineSend)",
-      date: "June 2015 to May 2019",
-      description: "Lead Front End Developer",
-      location: "Toronto, ON",
-      points: ["React", "Redux", "Vue", "SASS", "Laravel"],
-    },
-    {
-      title: "Freelance",
-      date: "January 2013 to June 2015",
-      description: "Web Developer",
-      location: "Orillia, ON",
-      points: ["Wordpress", "Drupal", "jQuery"],
-    },
-  ];
+/** @format */
 
-  export const recentProjects = [
-    {
-      title: "Analyze.Golf",
-      description: "Browser-based golf swing analyzer built with React, Redux, Tailwind and Konva.js.",
-      points: ["React", "Redux", "Vite", "Tailwind", "Konva.js"],
-      url: "https://www.analyze.golf/",
-      git: "https://github.com/tlouth19/analyze.golf"
-    },
-    {
-      title: "Lake St. George Golf Club",
-      description: "Website development and design using Next.js. All data is fetched from Twitter and Instagram APIs inside React server components, using the new Next.js app directory. I also handled all videos and photography.",
-      points: ["Next.js", "Tailwind", "Photography", "Videography"],
-      url: "https://www.lakestgeorgegolf.com/"
-    }
-  ]
+export const experiences = [
+  {
+    title: "Zebel",
+    date: "August 2022 to Present",
+    description: "Senior Front End Developer",
+    location: "San Francisco, CA (Remote)",
+    points: ["React", "Material UI"],
+  },
+  {
+    title: "Commerce7",
+    date: "June 2020 to August 2022",
+    description: "Senior Front End Developer",
+    location: "Vancouver, BC (Remote)",
+    points: [
+      "React",
+      "Redux",
+      "Node.js",
+      "Gatsby",
+      "Next.js",
+      "Styled Components",
+    ],
+  },
+  {
+    title: "Caredove",
+    date: "October 2019 to June 2020",
+    description: "Senior Front End Developer",
+    location: "Orillia, ON (Remote)",
+    points: [
+      "React",
+      "Redux",
+      "Node.js",
+      "Next.js",
+      "Tailwind",
+      "Styled Components",
+    ],
+  },
+  {
+    title: "Teaching.com",
+    date: "May 2019 to October 2019",
+    description: "Product Developer",
+    location: "San Juan, Puerto Rico (Remote)",
+    points: ["React", "Redux", "SASS", "Zend"],
+  },
+  {
+    title: "BitCine (CineSend)",
+    date: "June 2015 to May 2019",
+    description: "Lead Front End Developer",
+    location: "Toronto, ON",
+    points: ["React", "Redux", "Vue", "SASS", "Laravel"],
+  },
+  {
+    title: "Freelance",
+    date: "January 2013 to June 2015",
+    description: "Web Developer",
+    location: "Orillia, ON",
+    points: ["Wordpress", "Drupal", "jQuery"],
+  },
+];
+
+export const recentProjects = [
+  {
+    title: "Analyze.Golf",
+    description:
+      "Browser-based golf swing analyzer built with React, Redux, Tailwind and Konva.js.",
+    url: "https://www.analyze.golf/",
+    git: "https://github.com/tlouth19/analyze.golf",
+  },
+  {
+    title: "Golf Scoring App",
+    description: "A golf league scoring app built with Next.js 13.",
+    url: "https://mensnight.lakestgeorgegolf.com/",
+  },
+  {
+    title: "Lake St. George Golf Club",
+    description:
+      "Marketing website built with Next.js 13. Also handled all photography and videography.",
+    url: "https://www.lakestgeorgegolf.com/",
+  },
+];
